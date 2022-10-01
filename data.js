66c9a2a3c04c9ca4be98a8d25f632dcf373e5433
MaichelSameh <maichelsameh622@gmail.com>
2022-10-01 17:05:08 +0200

    adding the portfolio section

---
+++ data.js
@@ -0,0 +1,131 @@
+const projects = [
+    {
+        "id": "brandy-one",
+        "filter": "app",
+        "title": "Brandy one",
+        "description": `BrandyOne is an application that helps people to find luxury and well-known brands.<br><br>
+        The app is based in three countries: Kuwait, UAE, Saudi Arabia<br><br>
+        App feature:<br>
+        -E-commerce<br>
+        -online cart<br>
+        -offline cart<br>
+        -online payment<br>
+        -ads`,
+        "main-media-url": "assets/img/portfolio/brandy-one.png"
+    },
+    {
+        "id": "sa-dan",
+        "filter": "app",
+        "title": "Sa-dan",
+        "description": `Sa-dan is an online app to browse daily home needs.<br><br>
+        From this app, you can browse restaurants, shops, supermarkets, or any other type of shop.`,
+        "main-media-url": "assets/img/portfolio/sa-dan.png"
+    },
+    {
+        "id": "black-point",
+        "filter": "app",
+        "title": "Black point",
+        "description": `Abayat E-commerce application.<br>
+        <br>
+        Where is the secure or most famous platform to shop an Abaya? Simply NO.<br>
+        <br>
+        This was the idea of the client, creating a secure, famous, easy-to-reach platform to pay or shop all the abayat.`,
+        "main-media-url": "assets/img/portfolio/black-point.png"
+    },
+    {
+        "id": "entaji",
+        "filter": "app",
+        "title": "Entaji",
+        "description": `Project description
+        A complete E-commerce platform based in Saudi Arabia.<br>
+        App feature:<br>
+        - shopping<br>
+        - cart<br>
+        - online payment<br>
+        - seller dashboard<br>
+        - wishlist`,
+        "main-media-url": "assets/img/portfolio/entaji.png"
+    },
+    {
+        "id": "island-auto",
+        "filter": "app",
+        "title": "Island auto",
+        "description": ``,
+        "main-media-url": "assets/img/portfolio/island-auto.png"
+    },
+    {
+        "id": "my-boarding",
+        "filter": "app",
+        "title": "My Boarding",
+        "description": `"Why should I check the weather from an application and check the currency market from another app?"<br>
+        This was the problem that the client faced for a long time. The solution was very simple ("My boarding")<br>
+        <br>
+        An application that simplifies the process of checking the weather and the currency every day.<br>
+        <br>
+        Want to check the weather and the currency in the other country?<br>
+        Open the app and search for the country, you got the weather, go to the Currencies section, select the currency you want, and you got the currency for the other country :)`,
+        "main-media-url": "assets/img/portfolio/my-boarding.png"
+    },
+    {
+        "id": "submit",
+        "filter": "app",
+        "title": "Submit",
+        "description": `
+        E-learning application for universities.<br>
+        The app uses the video streaming feature so that the professors can upload their lessons and courses to all the students`,
+        "main-media-url": "assets/img/portfolio/submit-edu.png"
+    },
+];
+
+const testimonial = [
+    {
+        "name": "Mohammed Alaa",
+        "testimonial": "Maichel is awesome on technical level same as personal level too, Maichel is always cooperator and follow-up with you on every single details in your project with up-to-dated details on what is happening right now.",
+        "role": "Software Engineer",
+    },
+    {
+        "name": "Moussa gerges",
+        "testimonial": "I am very glad to have worked with you Maichel on a multidisciplinary project to develop mobile applications and connect them to my IOT project via secure APIs. You have been very serious and punctual, so I hope to work together again soon.",
+        "role": "IOT Developer",
+    },
+    {
+        "name": "Abbdelrahman Ashraf",
+        "testimonial": "Maichel is really a talented developer and follows best practices writing and organizing his code 👏🏻💪🏻",
+        "role": "Flutter Developer",
+    },
+    {
+        "name": "George Aziz",
+        "testimonial": "Michael is one of the cleverest mobile app developer that I have ever seen. As a UI/UX Designer, I'm always concerned about developing the application interfaces just like the design without any changes in the elements design, spacing,  animation, etc... But Michael did the job very well!",
+        "role": "UI/UX Designer",
+    },
+];
+
+const jobs = [
+    {
+        "title": "Junior Flutter developer",
+        "date": "2022 - Present",
+        "company": "Mix-code",
+        "role": "Freelancer",
+        "responsibilities": [
+            "create multi-platform apps for iOS and Android using Google's Flutter development framework",
+            "Design and develop high-volume, low-latency applications and deliver high availability and performance.",
+            "Create well designed, testable, efficient code",
+            "Participate in all phases of the development life cycle",
+            "API Integration",
+            "Troubleshooting and debugging to optimize performance.",
+            "Work with the product and design teams to understand end-user requirements and use cases and translate them to pragmatic and effective technical solutions",
+        ],
+    },
+    {
+        "title": "Junior Flutter developer",
+        "date": "2021 - 2021",
+        "company": "Arab Badia",
+        "role": "Freelancer",
+        "responsibilities": [
+            "Developing Multi-platform application",
+            "evolving the software life cycle",
+            "API integration",
+            "Work with the product and design teams to understand end-user requirements and use cases and translate them to pragmatic and effective technical solutions",
+        ],
+    },
+];
